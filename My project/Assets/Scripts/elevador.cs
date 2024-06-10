9ae0a48721202708926b551d209a4cd2779e5944
Lucas Thurler <lucasthurlergoncalves@gmail.com>
2024-06-09 22:14:27 -0300

    scriptElevador

---
+++ My project/Assets/Scripts/elevador.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevador : MonoBehaviour
+{
+    int andar, tempo, numero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Andar começa no primeiro
+        // Contador de tempo da fase começa a cair (5 minutos será o tempo de tela)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If jogador aperta um botão de andar no painel
+            // Elevador sobe em um determinado tempo, passando pelos andares 
+            // (podemos assumir 3 segundos entre os andares)
+        // If numero == andar escolhido
+            // Elevador para e o monstro desce
+            // + pontos
+        // Elevador continua a partir do andar em que parou
+    }
+}
