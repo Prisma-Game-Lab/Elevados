3fe610b6bda87fc5a1947a0192486321cb3f944e
João Marcello <marcello.jao1611@gmail.com>
2024-06-09 22:45:34 -0300

    Objeto audio manager criado

---
+++ My project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] musicas, efeitosSonoros;
+    [SerializeField]
+    private AudioSource musicSource, SFXSource;
+
+    public void TocaMusica(string nomeMusica)
+    {
+        AudioClip musica = Array.Find(musicas, x => x.name == nomeMusica);
+
+        if (musica == null)
+        {
+            Debug.Log("Música não encontrada");
+            return;
+        }
+
+        musicSource.clip = musica;
+        musicSource.Play();
+    }
+
+    public void TocaEfeitoSonoro(string nomeEfeito)
+    {
+        AudioClip efeito = Array.Find(musicas, x => x.name == nomeEfeito);
+
+        if (efeito == null)
+        {
+            Debug.Log("Música não encontrada");
+            return;
+        }
+
+        SFXSource.clip = efeito;
+        SFXSource.Play();
+    }
+
+    void Start()
+    {
+        if (gameObject != null)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
