ef57b2f510771c73c7bc56ca4781300624996765
João Marcello <marcello.jao1611@gmail.com>
2024-07-08 13:59:57 -0300

    monstro monstra qual andar ele quer ir, mas creio q tem um bug no monster manager q às vezes tá mostrando o andar atual

--- My project/Assets/Scripts/Monsters/Monster.cs
+++ My project/Assets/Scripts/Monsters/Monster.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class Monster : MonoBehaviour
 {
     public int CurrentFloor { get; private set; }
     public int TargetFloor { get; private set; }
+    private GameObject bolhaDeFala;
+
+    private void OnEnable()
+    {
+        bolhaDeFala = GameObject.Find("Canvas").transform.GetChild(1).gameObject;
+        bolhaDeFala.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = TargetFloor.ToString();
+        bolhaDeFala.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        bolhaDeFala.SetActive(false);
+    }
 
     public void Initialize(int currentFloor, int targetFloor)
     {
