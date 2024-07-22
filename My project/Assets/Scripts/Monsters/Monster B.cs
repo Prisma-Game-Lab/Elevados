bc750b3f5f8d0284ba147f370a311eeb1ce0de92
João Marcello <marcello.jao1611@gmail.com>
2024-07-22 20:19:29 -0300

    Monstro B feito, falta só rotacionar o monstro em si (consertar quando o gerador de monstros for feito)

--- My project/Assets/Scripts/Monsters/Monster B.cs
+++ My project/Assets/Scripts/Monsters/Monster B.cs
@@ -5,21 +5,17 @@ using UnityEngine;
 
 public class MonsterB : Monster
 {
-    private List<GameObject> objetosPraInverter = new();
+    private GameObject[] objetosPraInverter;
     private void Start()
     {
-        objetosPraInverter.Add(GameObject.FindWithTag("Background"));
-        objetosPraInverter.Add(GameObject.Find("Canvas").transform.GetChild(0).gameObject);
-
-        InverteObjetos();
+        objetosPraInverter = GameObject.FindGameObjectsWithTag("Background");
     }
 
     public void InverteObjetos()
     {
-        for (int i = 0; i < objetosPraInverter.Count; i++)
+        foreach (GameObject objeto in objetosPraInverter)
         {
-            var atual = objetosPraInverter[i];
-            atual.transform.localScale = new Vector3(atual.transform.localScale.x, atual.transform.localScale.y * -1, atual.transform.localScale.z);
+            objeto.transform.Rotate(0.0f, 0.0f, 180.0f);
         }
     }
 
