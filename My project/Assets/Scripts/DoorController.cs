bdd664b50ea3cc03789234f13acbf57a0328d5a1
Lucas Thurler <lucasthurlergoncalves@gmail.com>
2024-07-24 17:53:13 -0300

    ajustes dentro do unity e correcao do animator controller

--- My project/Assets/Scripts/DoorController.cs
+++ My project/Assets/Scripts/DoorController.cs
@@ -6,7 +6,6 @@ public class DoorController : MonoBehaviour
 {
     public Animator doorAnimatorEsquerda;
     public Animator doorAnimatorDireita;
-    public Animator Grade; // Adicione o Animator para a grade
     public MonsterManager monsterManager;
 
     private bool isMoving = false;
@@ -16,7 +15,6 @@ public class DoorController : MonoBehaviour
         // Certifique-se de que a porta começa aberta
         doorAnimatorEsquerda.SetTrigger("Abrir 0");
         doorAnimatorDireita.SetTrigger("Abrir 0");
-        Grade.SetTrigger("Aberto"); // Adicione a animação inicial para a grade
     }
 
     public void MoveElevator(int targetFloor, GameObject novo)
@@ -34,7 +32,6 @@ public class DoorController : MonoBehaviour
         // Fechar a porta e a grade antes de mover
         doorAnimatorEsquerda.SetTrigger("Fechar");
         doorAnimatorDireita.SetTrigger("Fechar");
-        Grade.SetTrigger("Fecha"); // Adicione a animação de fechar a grade
         yield return new WaitForSeconds(2); // Tempo da animação de fechar a porta e a grade
 
         // Simula a movimentação do elevador (ajuste conforme necessário)
@@ -44,7 +41,6 @@ public class DoorController : MonoBehaviour
         // Abrir a porta e a grade ao chegar no andar
         doorAnimatorEsquerda.SetTrigger("Abrir");
         doorAnimatorDireita.SetTrigger("Abrir");
-        Grade.SetTrigger("Abre"); // Adicione a animação de abrir a grade
         yield return new WaitForSeconds(2); // Tempo da animação de abrir a porta e a grade
 
         // Notifique o MonsterManager sobre o andar que foi alcançado
